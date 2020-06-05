e5d7b4cce247b3bd121261eba1503957be17a601
Sanyo <sanyo.gorbunov@gmail.com>
2020-06-06 01:55:45 +0200

    Added music and switch on off

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    private bool isMute = false;
+    public AudioSound[] sounds;
+    // Start is called before the first frame update
+
+    public static AudioManager instance = null;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+
+        foreach (AudioSound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("Xythe");
+    }
+
+    public void Play(string name)
+    {
+        AudioSound sound = Array.Find(sounds, s => s.name == name);
+        sound.source.Play();
+    }
+
+    public void ToggleMute()
+    {
+        isMute = !isMute;
+        SetMute(isMute);
+    }
+
+    public void SetMute(bool isMute)
+    {
+        if (isMute)
+        {
+            Mute();
+        }
+        else {
+            UnMute();
+        }
+    }
+
+
+    public void Mute()
+    {
+        foreach (AudioSound s in sounds)
+        {
+            s.source.volume = 0.0f;
+        }
+    }
+
+    public void UnMute()
+    {
+        foreach (AudioSound s in sounds)
+        {
+            s.source.volume = s.volume;
+        }
+    }
+    public static void StaticPlay(string name)
+    {
+        instance.Play(name);
+    }
+
+    public void Pause(string name)
+    {
+        AudioSound sound = Array.Find(sounds, s => s.name == name);
+        sound.source.Pause();
+    }
+
+    public static void StaticPause(string name)
+    {
+        instance.Pause(name);
+    }
+
+    public void UnPause(string name)
+    {
+        AudioSound sound = Array.Find(sounds, s => s.name == name);
+        sound.source.UnPause();
+    }
+
+    public static void StaticUnpause(string name)
+    {
+        instance.UnPause(name);
+    }
+}
