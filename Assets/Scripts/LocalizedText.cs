724fbfe5ce792d704c2e4b5ac23af840aec2ce27
Sanyo <sanyo.gorbunov@gmail.com>
2020-06-05 23:07:44 +0200

    Added locales

---
+++ Assets/Scripts/LocalizedText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizedText : MonoBehaviour
+{
+    public string Key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var textObject = gameObject.GetComponent<Text>();
+        if (textObject != null)
+        {
+            textObject.text = Mgl.I18n.Instance.__(Key);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
