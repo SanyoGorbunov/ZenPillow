12be011cdafda741c20822949e660633d619ca51
Dmitry <divgorbunov@gmail.com>
2020-06-06 13:31:47 +0300

    Add ToggleGroupController

---
+++ Assets/Scripts/ToggleGroupController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleGroupController : MonoBehaviour
+{
+    Dictionary<int, bool> answers = new Dictionary<int, bool>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSelected(int id, bool isSelected)
+    {
+        answers.Add(id, isSelected);
+    }
+
+    public void checkResult()
+    {
+    }
+}
