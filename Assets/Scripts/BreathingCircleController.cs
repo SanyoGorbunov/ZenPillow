fc425a08b0b1419919f1d734571f8b929f1e66a1
Sanyo <sanyo.gorbunov@gmail.com>
2020-06-06 23:08:05 +0200

    Init breathing

---
+++ Assets/Scripts/BreathingCircleController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BreathingCircleController : MonoBehaviour
+{
+    private const float TimeInSecs = 6.0f;
+
+    private GameObject _innerCircle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _innerCircle = GameObject.FindGameObjectWithTag("InnerCircle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Scale(bool isUpscaling, Action<bool> callback)
+    {
+        StartCoroutine(ScaleOverTime(isUpscaling, callback));
+    }
+
+    IEnumerator ScaleOverTime(bool isUpscaling, Action<bool> callback)
+    {
+        Vector3 originalScale = isUpscaling ? new Vector3(0.2f, 0.2f, 1.0f) : new Vector3(1.0f, 1.0f, 1.0f);
+        Vector3 destinationScale = !isUpscaling ? new Vector3(0.2f, 0.2f, 1.0f) : new Vector3(1.0f, 1.0f, 1.0f);
+
+        float currentTime = 0.0f;
+
+        do
+        {
+            _innerCircle.transform.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / TimeInSecs);
+            currentTime += Time.deltaTime;
+            yield return null;
+        } while (currentTime <= TimeInSecs);
+
+        callback.Invoke(isUpscaling);
+    }
+}
---
+++ Assets/Scripts/BreathingController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BreathingController : MonoBehaviour
+{
+    public BreathingCircleController breathingCircleController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(nameof(Play));
+    }
+
+    IEnumerator Play()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Inhale();
+    }
+
+    void Inhale()
+    {
+        breathingCircleController.Scale(true, isUpscaling => Exhale());
+    }
+
+    void Exhale()
+    {
+        breathingCircleController.Scale(false, isUpscaling => Inhale());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
