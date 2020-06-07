9209af1e589568bfe358b984d5c4babf9bb00360
Sanyo <sanyo.gorbunov@gmail.com>
2020-06-07 21:19:05 +0200

    Localized questionnaire

--- Assets/Scripts/QuestionController.cs
+++ Assets/Scripts/QuestionController.cs
@@ -7,7 +7,8 @@ public class QuestionController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var text = GetComponentInChildren<LocalizedText>();
+        text.SetupKey(question.ToString());
     }
 
     // Update is called once per frame
