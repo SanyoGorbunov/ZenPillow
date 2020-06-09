b4362157f0004d5d6e6e85c6e70ce811e7aa3066
Dmitry <divgorbunov@gmail.com>
2020-06-10 02:29:45 +0300

    Add overlay to scene to make ui fade-in / fade-out animation

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,34 +1,69 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject overlay;
+
+    public delegate void del();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(AnimateAlpha(1, 0, 0.5f, new del(() => { overlay.SetActive(false); })));
     }
 
     public void SelectPractice()
     {
-        SceneManager.LoadScene("SelectPracticeScene");
+        overlay.SetActive(true);
+        StartCoroutine(AnimateAlpha(0, 1, 0.5f, new del(() => {  SceneManager.LoadScene("SelectPracticeScene"); })));
     }
 
     public void OpenQuestionnaire()
     {
-        SceneManager.LoadScene("Questionnaire");
+        overlay.SetActive(true);
+        StartCoroutine(AnimateAlpha(0, 1, 0.5f, new del(() => { SceneManager.LoadScene("Questionnaire"); })));
     }
 
     public void OpenFavoriteRoom()
     {
         var practice = GameStateManager.Instance.GetFavoritePractice();
         GameStateManager.Instance.SelectPractice(practice);
-        SceneManager.LoadScene("TimerScene");
+
+        overlay.SetActive(true);
+        StartCoroutine(AnimateAlpha(0, 1, 0.5f, new del(() => { SceneManager.LoadScene("TimerScene"); })));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator AnimateAlpha(float startValue, float endValue, float duration, del action)
+    {
+        float elapsedTime = 0;
+        float ratio = elapsedTime / duration;
+        while (ratio < 1f)
+        {
+            elapsedTime += Time.deltaTime;
+            ratio = elapsedTime / duration;
+
+            float alpha = startValue + (endValue - startValue) * ratio;
+
+            Color temp = Color.white;
+            temp.a = alpha;
+
+            overlay.GetComponent<Image>().color = temp;
+
+            yield return null;
+        }
+
+        if (action != null)
+        {
+            action();
+        }        
     }
 }
