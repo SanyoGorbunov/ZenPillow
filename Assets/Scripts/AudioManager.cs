b474704a0c8c04380b7ec6e4f362a2e602a31843
Sanyo <sanyo.gorbunov@gmail.com>
2020-09-06 23:32:41 +0200

    Added basic sound change func

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System;
+using Boo.Lang;
 
 public class AudioManager : MonoBehaviour
 {
     private bool isMute = false;
     public AudioSound[] sounds;
     // Start is called before the first frame update
+
+    private string[] _backgroundSounds;
+    private string _currentBackgroundSound = "Ambient";
 
     public static AudioManager instance = null;
     void Awake()
@@ -22,7 +26,7 @@ public class AudioManager : MonoBehaviour
 
         DontDestroyOnLoad(gameObject);
 
-
+        var backgroundSounds = new List<string>();
         foreach (AudioSound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -31,12 +35,33 @@ public class AudioManager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.clip = s.clip;
             s.source.loop = s.loop;
+
+            if (s.isBack)
+            {
+                backgroundSounds.Add(s.name);
+            }
         }
+
+        _backgroundSounds = backgroundSounds.ToArray();
+    }
+
+    public string[] GetBackgroundSounds()
+    {
+        return _backgroundSounds;
     }
 
     private void Start()
     {
-        Play("Ambient");
+        Play(_currentBackgroundSound);
+    }
+
+    public void Change(string name)
+    {
+        AudioSound sound = Array.Find(sounds, s => s.name == _currentBackgroundSound);
+        sound.source.Stop();
+
+        _currentBackgroundSound = name;
+        Play(_currentBackgroundSound);
     }
 
     public void Play(string name)
---
+++ Assets/SoundPickerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundPickerController : MonoBehaviour
+{
+    private string[] _sounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var dropdown = gameObject.GetComponent<Dropdown>();
+        _sounds = AudioManager.instance.GetBackgroundSounds();
+        dropdown.options = _sounds.Select(sound => new Dropdown.OptionData(sound)).ToList();
+    }
+
+    public void OnValueChanged(int index)
+    {
+        AudioManager.instance.Change(_sounds[index]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
